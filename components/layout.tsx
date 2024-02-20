19f93c2b67e0c3577c4719f88f3861e8ee46ee49
arthur revelski <arevelski@managedmethods.com>
2024-02-19 18:50:11 -0700

    updated google tag manager references

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import Head from "next/head";
 import ThemeToggle from "./Header/themeToggle";
 import Header from "./Header/Header";
+import Head from "next/head";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
 
 export const siteTitle = "Arthur Revelski";
 
-export default function Layout({
-  children,
-  home,
-}: {
-  children: React.ReactNode;
-  home?: boolean;
-}) {
+export default function Layout({ children }: LayoutProps) {
   const [theme, setTheme] = useState("light");
+
   return (
     <>
       <Head>
@@ -23,14 +23,13 @@ export default function Layout({
       </Head>
       <div className={theme}>
         <Header />
-        <main
-          id="content"
-          className=" lg:float-right md:float-none lg:w-8/12 md:w-12/12 py-12 px-16 sm:px-24 dark:bg-zinc-900"
-        >
+        <main className="lg:float-right md:float-none lg:w-8/12 md:w-12/12 py-12 px-16 sm:px-24 dark:bg-zinc-900">
           <ThemeToggle config={{ theme, setTheme }} />
           {children}
         </main>
       </div>
+      <GoogleTagManager gtmId="GTM-MPNWC3" />
+      <GoogleAnalytics gaId="G-E1F12C8Q41" />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,19 @@ import Layout from "../components/layout";
 import Experience from "../components/Experience/Experience";
 import Education from "../components/Education/Education";
 import TechnicalSkills from "../components/TechinalSkills/TechnicalSkills";
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 const index: NextPage = () => {
   return (
-    <Layout home>
-        <section className="max-w-xl mb-8">
-            <Experience/>
-        </section>
-        <section className="max-w-xl mb-8">
-        <TechnicalSkills/>
-        </section>
-        <section className="max-w-xl mb-8">
-            <Education/>
-        </section>
-        <GoogleAnalytics gaId="G-E1F12C8Q41"/>
+    <Layout>
+      <section className="max-w-xl mb-8">
+        <Experience />
+      </section>
+      <section className="max-w-xl mb-8">
+        <TechnicalSkills />
+      </section>
+      <section className="max-w-xl mb-8">
+        <Education />
+      </section>
     </Layout>
   );
 };
